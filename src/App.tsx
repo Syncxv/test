ab7dd3ced6688ea8e748580425ace9964746d35f
Syncxv <aruldevarackal1@gmail.com>
2023-05-28 06:50:22 +1000

    oops

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { invoke } from "@tauri-apps/api/tauri";
 function App() {
 	return (
 		<div style="display: flex; height: 100vh; align-items:center; justify-content: center; gap: 1rem">
-			<button onClick={async () => console.log(await invoke("greet"))}>
+			<button
+				onClick={async () =>
+					console.log(await invoke("greet", { name: "bruh" }))
+				}
+			>
 				greet
 			</button>
 			<button onClick={async () => console.log(await invoke("get_config"))}>
